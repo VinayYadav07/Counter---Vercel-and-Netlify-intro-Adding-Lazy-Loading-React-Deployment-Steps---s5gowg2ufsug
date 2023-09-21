95f6aad035adbc034e80d129fb1344293abd5dde
VinayYadav07 <98084463+VinayYadav07@users.noreply.github.com>
2023-09-21 15:34:10 +0530

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
-import React, { Component } from "react";
-import Counter from "./counterContainer";
+import React from 'react';
+import { connect } from 'react-redux'; // Import connect from react-redux to connect your component to the Redux store
+import { incrementCount, decrementCount, resetCount } from '../actions'; // Import your action creators
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <Counter />
-                </header>
-            </div>
-        );
-    }
+class CounterContainer extends React.Component {
+  render() {
+    const { count, incrementCount, decrementCount, resetCount } = this.props;
+
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button onClick={incrementCount}>Increment</button>
+        <button onClick={decrementCount}>Decrement</button>
+        <button onClick={resetCount}>Reset</button>
+      </div>
+    );
+  }
 }
-export default App;
+
+// Map the Redux state to the component props
+const mapStateToProps = (state) => {
+  return {
+    count: state.count, // Assuming your Redux state has a property called "count"
+  };
+};
+
+// Map the action creators to the component props
+const mapDispatchToProps = {
+  incrementCount,
+  decrementCount,
+  resetCount,
+};
+
+// Connect the component to the Redux store
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
